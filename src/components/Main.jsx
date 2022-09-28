2f2d3b760b30ccc8b11351a2fbd52a48577011e8
Bernat Escarrà <bernat.escarra@gmail.com>
2022-09-28 16:19:36 +0200

    Update styles

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,5 @@
 export default function Main({ children }) {
   return (
-    <main className="flex-1 container mx-auto">{children}</main>
+    <main className="flex-1 container mx-auto flex flex-col">{children}</main>
   )
 }
