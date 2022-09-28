41aad70ad776459ebee5bc23ecd9c28047c78181
Bernat Escarrà <bernat.escarra@gmail.com>
2022-09-28 10:44:04 +0200

    Add main functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { useState, useEffect } from 'react'
+import { getPeople } from './services/swapi.services'
+
 export default function App() {
+  const [people, setPeople] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    getPeople()
+      .then(res => setPeople(res))
+      .then(() => setIsLoading(false))
+      .catch(err => console.log(err))
+  }, [])
+
   return (
+    <>
     <div className="flex flex-col items-center mt-10">
       <h2 className="text-xl">Welcome to</h2>
       <h1 className="text-4xl font-bold">React Wars</h1>
     </div>
+    {isLoading ? 'Loading' : JSON.stringify(people)}
+    </>
   )
 }
