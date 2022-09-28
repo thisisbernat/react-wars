b1a342afd02cc8f31eeb2ad1028c39b377b909d9
Bernat Escarrà <bernat.escarra@gmail.com>
2022-09-28 09:20:53 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="flex flex-col items-center mt-10">
+      <h2 className="text-xl">Welcome to</h2>
+      <h1 className="text-4xl font-bold">React Wars</h1>
+    </div>
+  )
+}
